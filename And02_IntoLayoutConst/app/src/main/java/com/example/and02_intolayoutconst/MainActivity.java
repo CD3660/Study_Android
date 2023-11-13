571fcf3cb95f070c7d2171f6ce269922d49f9615
박찬영 <144075327+CD3660@users.noreply.github.com>
2023-11-13 13:06:09 +0900

    231113

---
+++ And02_IntoLayoutConst/app/src/main/java/com/example/and02_intolayoutconst/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.and02_intolayoutconst;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+//        Java코드에서 res 하위 layout파일을 선택 후 연결한다.
+//        setContentView(R.layout.activity_main);
+    }
+}
